8640f430c741c679ecb7aad93d621b5967a1dd7f
daysandmoons1 <70629221+daysandmoons1@users.noreply.github.com>
2021-12-07 01:50:33 +0900

    choice manager

---
+++ Assets/Sunhyo/Scripts/ChoiceManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceManager : MonoBehaviour
+{
+    public RectTransform midPanel;
+    public RectTransform bottomPanel;
+    public Scrollbar scrollBar;
+    public GameObject ChatBar;
+    public GameObject Choices;
+
+    public void ShowChoice()
+    {
+        ChatBar.SetActive(false);
+
+        midPanel.anchoredPosition = new Vector2(0, 220);
+        midPanel.sizeDelta = new Vector2(0, 1040);
+        bottomPanel.sizeDelta = new Vector2(0, 640);
+        
+        Choices.SetActive(true);
+
+        scrollBar.value = 0f;
+    }
+
+    public void CloseChoice()
+    {
+        Choices.SetActive(false);
+
+        midPanel.anchoredPosition = new Vector2(0, -25);
+        midPanel.sizeDelta = new Vector2(0, 1570);
+        bottomPanel.sizeDelta = new Vector2(0, 150);
+
+         ChatBar.SetActive(true);
+
+        scrollBar.value = 0f;
+    }
+}
