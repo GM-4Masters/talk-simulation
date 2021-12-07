d5021c06dd8053d9582f363baa4bf12d6b64425c
daysandmoons1 <70629221+daysandmoons1@users.noreply.github.com>
2021-12-07 18:15:23 +0900

    이미지 및 폰트 적용

--- Assets/Sunhyo/Scripts/ChatManager.cs
+++ Assets/Sunhyo/Scripts/ChatManager.cs
@@ -10,16 +10,66 @@ public class ChatManager : MonoBehaviour
     public RectTransform contentRect;
     public Scrollbar scrollBar;
 
+    public RectTransform ChatScreen;
+    public RectTransform bottomPanel;
+    public GameObject ChatBar;
+    public GameObject Choices;
+
     public void Chat(bool isSend, string text, string name)
     {
         BubbleScript Bubble = Instantiate(isSend ? playerBubble : npcBubble).GetComponent<BubbleScript>();
         Bubble.transform.SetParent(contentRect.transform, false);
         Bubble.TextRect.GetComponent<Text>().text = text;
 
-
         scrollBar.value = 0f;
         Fit(Bubble.BoxRect);
     }
 
     void Fit(RectTransform Rect) => LayoutRebuilder.ForceRebuildLayoutImmediate(Rect);
+
+    public void ShowChoice()
+    {
+        ChatBar.SetActive(false);
+
+        ChatScreen.anchoredPosition = new Vector2(0, 145);
+        ChatScreen.sizeDelta = new Vector2(0, 880);
+        bottomPanel.sizeDelta = new Vector2(0, 640);
+
+        Choices.SetActive(true);
+
+        scrollBar.value = 0f;
+    }
+
+    public void CloseChoice()
+    {
+        Choices.SetActive(false);
+
+        ChatScreen.anchoredPosition = new Vector2(0, -100);
+        ChatScreen.sizeDelta = new Vector2(0, 1370);
+        bottomPanel.sizeDelta = new Vector2(0, 150);
+
+        ChatBar.SetActive(true);
+
+        scrollBar.value = 0f;
+    }
+
+    public void FisrtChoice()
+    {
+        print("첫번째");
+    }
+
+    public void SecondChoice()
+    {
+        print("두번째");
+    }
+
+    public void ThirdChoice()
+    {
+        print("세번째");
+    }
+
+    public void BackToChatList()
+    {
+        print("리스트로 이동");
+    }
 }
